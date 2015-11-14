8aa2759dc18c4194db1bfb24b9442793952923b8
Liam Andrew <landrew@texastribune.org>
2015-11-14 11:28:02 -0600

    Get form skeleton working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+function validateInputSize() {
+    if ($(this)[0].value.length > 140) {
+        $(this).parent().parent().addClass('has-error');
+    }
+    else {
+        $(this).parent().parent().addClass('has-success');
+    }
+};
 
+$(document).ready(function() {
+    $('#inputReason').on('keyup', validateInputSize);
+});

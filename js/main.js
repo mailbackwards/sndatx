18235887ee61b8fc9a7edad08ffe836808dc0a8a
Liam Andrew <landrew@texastribune.org>
2015-11-14 15:02:03 -0600

    Start work on twitter+youtube oembed

--- js/main.js
+++ js/main.js
@@ -7,6 +7,52 @@ function validateInputSize() {
     }
 };
 
+function renderYoutube(url) {
+
+}
+function renderTwitter(url) {
+    console.log('HERE');
+    $.getJSON('http://api.twitter.com/1/statuses/oembed.json?callback=?&url='+url, {
+        format: 'json',
+        dataType: 'jsonp',
+    })
+    .done(function(data) {
+        return data.html;
+    });
+}
+
+function scrapeLink() {
+    event.preventDefault();
+    var userName = $('#inputUserName').val();
+        userEmail = $('#inputUserEmail').val();
+        link = $('#inputLink').val();
+        reason = $('#inputReason').val();
+        embed = '';
+
+    if (link.indexOf('youtube.com') > -1 || link.indexOf('youtu.be') > -1) {
+        $.getJSON('https://noembed.com/embed?callback=?', {
+                format: 'json',
+                url: link
+            })
+            .done(function(data) {
+                embed = data.html;
+            })
+    } else if (link.indexOf('twitter.com') > -1 || link.indexOf('t.co') > -1) {
+        $.getJSON('http://api.twitter.com/1/statuses/oembed.json?callback=?&url='+link, {
+                format: 'json',
+                dataType: 'jsonp',
+            })
+            .done(function(data) {
+                embed = data.html;
+            });
+    } else {
+        embed = "<div><p>"+link+"</p></div>";
+        console.log(embed);
+    }
+    //console.log(embed);
+};
+
 $(document).ready(function() {
     $('#inputReason').on('keyup', validateInputSize);
+    $('#submitNewLink').on('click', scrapeLink);
 });
